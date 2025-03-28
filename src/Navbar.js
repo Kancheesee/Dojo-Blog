3fd4fa5aab6bd11d088506be7aa2070054cae22f
Kancheesee <kanchi.kumari.che23@itbhu.ac.in>
2025-03-29 01:34:03 +0530

    added click button

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
             <h1>The Dojo Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create"></a>
+                <a href="/create" style={{
+                    color:"white",
+                    backgroundColor:"#f1356d",
+                    borderRadius:"5px"
+                }}>New Blog</a>
             </div>
         </nav>
     );
