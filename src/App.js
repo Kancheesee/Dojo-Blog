f461099dbb7d74f9ab5db17691f3dcf2a3d12d12
Kancheesee <kanchi.kumari.che23@itbhu.ac.in>
2025-04-04 16:51:40 +0530

    added not found page and done!!!

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 function App() {
   
     
@@ -23,6 +24,10 @@ function App() {
 
             <Route path="/create" element={<Create/>}>
           </Route>
+
+          <Route path="*" element={<NotFound/>}>
+          </Route>
+          
         </Routes>
       </div>
     </div>
