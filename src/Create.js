52edff0f344ab8976eecf6784cec5ebfd66f9330
Kancheesee <kanchi.kumari.che23@itbhu.ac.in>
2025-04-04 16:43:23 +0530

    added delete button

--- src/Create.js
+++ src/Create.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import {useHistory} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 const Create = () => {
     const[title,setTitle]=useState('');
     const[body,setBody]=useState('');
     const[author, setAuthor]=useState('mario');
     const [isPending, setIsPending] =useState(false);
-    const history= useHistory();
+    const navigate= useNavigate();
 
     const handleSubmit=(e)=>{
         e.preventDefault();
@@ -22,9 +22,10 @@ const Create = () => {
             console.log('new blog added');
 
             setIsPending(false);
+            navigate(-1);
         })
 
-        history.go(-1)
+        
     }
 
     return ( 
